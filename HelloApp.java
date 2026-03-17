512df94e0ed1612f53ec247cb7da33b5ea955498
yashhopawat <yo9198@srmist.edu.in>
2026-03-17 15:05:53 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
 
-        // Check if user provided name
+        
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
         } else {
